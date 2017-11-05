ffb46a0d9fa366735ab5aab9a87a5ff4d0f056d7
Martin Hauskrecht <hauskrechtmartin@gmail.com>
2017-11-05 01:23:09 +0100

    INVEST-119: Created Singleton properties loader with getters for each propery

--- server/src/test/java/sk/ystad/ServerApplicationTests.java
+++ server/src/test/java/sk/ystad/ServerApplicationTests.java
@@ -1,16 +1,29 @@
 package sk.ystad;
 
+import org.apache.log4j.Logger;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import sk.ystad.common.properties.PropertiesLoader;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class ServerApplicationTests {
 
+    static Logger LOG = Logger.getLogger(ServerApplicationTests.class.getName());
+
 	@Test
 	public void contextLoads() {
 	}
 
+	@Test
+    public void propertiesLoad() {
+        PropertiesLoader loader = PropertiesLoader.getInstance();
+        String influxUser = loader.getInfluxUser();
+        String expectedUser = "invest";
+
+        Assert.assertEquals(influxUser, expectedUser);
+    }
 }
