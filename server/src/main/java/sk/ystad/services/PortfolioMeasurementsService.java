bfc7c288f8972f0539566477ff2519dfaa48111a
Tomas Gabrs <gabrstomas@gmail.com>
2017-11-08 13:11:01 +0100

    INVEST-135 Changed service path, parameters and response structure

---
+++ server/src/main/java/sk/ystad/services/PortfolioMeasurementsService.java
@@ -0,0 +1,47 @@
+package sk.ystad.services;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import sk.ystad.model.measures.database_objects.ImmutableMeasure;
+import sk.ystad.model.measures.repositores.PortfolioMeasureRepository;
+import sk.ystad.model.timeseries.database_objects.date.localdate.LocalDateDoubleTimeSeries;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+@RestController
+@RequestMapping("/measurements")
+public class PortfolioMeasurementsService {
+    private static DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+
+    private final PortfolioMeasureRepository portfolioMeasureRepository;
+
+    @Autowired
+    PortfolioMeasurementsService(PortfolioMeasureRepository portfolioMeasureRepository) {
+        this.portfolioMeasureRepository = portfolioMeasureRepository;
+    }
+
+    @GetMapping("/cumulativeMeasurement")
+    public String findByUserId(@RequestParam(value = "portfolioId") String portfolioId, @RequestParam(value="dateFrom") String dateFrom,
+                               @RequestParam(value="dateTo") String dateTo) throws Exception {
+        LocalDate localDateFrom = LocalDate.parse(dateFrom, formatter);
+        LocalDate localDateTo = LocalDate.parse(dateTo, formatter);
+
+        ImmutableMeasure immutableMeasure = ImmutableMeasure.of("PORTFOLIO_CUMULATIVE_RETURN");
+        LocalDateDoubleTimeSeries timeSeries = portfolioMeasureRepository.findMeasure(portfolioId, immutableMeasure, localDateFrom, localDateTo);
+
+        JSONArray dataJson = new JSONArray();
+
+        for (int i = 0; i < timeSeries.size(); i++) {
+            JSONObject tmpObject = new JSONObject();
+            tmpObject.put("date-time-stamp", timeSeries.times().get(i));
+            tmpObject.put("value", timeSeries.values().get(i));
+            dataJson.put(tmpObject);
+        }
+
+        return dataJson.toString();
+    }
+
+}
