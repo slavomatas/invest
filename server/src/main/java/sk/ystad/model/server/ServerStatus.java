496321390d803c78ef18b16beb2867149d1d365f
Martin Hauskrecht <hauskrechtmartin@gmail.com>
2017-11-08 15:27:16 +0100

    INVEST-138: Added simple get server status REST at /status.

---
+++ server/src/main/java/sk/ystad/model/server/ServerStatus.java
@@ -0,0 +1,14 @@
+package sk.ystad.model.server;
+
+public class ServerStatus {
+
+    private String status;
+
+    public ServerStatus() {
+        this.status = "running";
+    }
+
+    public String getStatus() {
+        return status;
+    }
+}
