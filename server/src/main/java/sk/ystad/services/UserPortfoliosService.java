508afee0ee6445c4545a686d7f469622ed5e4101
Martina Halajova <halajovamartina@gmail.com>
2017-11-14 16:49:06 +0100

    INVEST-136 Add swagger documentation for REST service "Get user portfolios"

--- server/src/main/java/sk/ystad/services/UserPortfoliosService.java
+++ server/src/main/java/sk/ystad/services/UserPortfoliosService.java
@@ -1,5 +1,6 @@
 package sk.ystad.services;
 
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import sk.ystad.model.users.database_objects.Portfolio;
@@ -22,6 +23,7 @@ public class UserPortfoliosService {
 
     @CrossOrigin(origins = "*")
     @GetMapping("/user/portfolios")
+    @ApiOperation(value = "Get all user's portfolios details", notes = "UserID is retrieved from session")
     public List<Portfolio> findByUserId(){
         //TODO: get user assigned to current session
         long userId = 3L;
