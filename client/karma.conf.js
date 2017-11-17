45ea448a4ad465280cfb7dfd79ef8f99df106742
Martin Hauskrecht <hauskrechtmartin@gmail.com>
2017-11-17 22:52:50 +0100

    INVEST-157: Updated package.json and added karma.conf

---
+++ client/karma.conf.js
@@ -0,0 +1,42 @@
+// Karma configuration file, see link for more information
+// https://karma-runner.github.io/0.13/config/configuration-file.html
+
+module.exports = function (config) {
+  config.set({
+    basePath: '',
+    frameworks: ['jasmine', '@angular/cli'],
+    plugins: [
+      require('karma-jasmine'),
+      require('karma-phantomjs-launcher'),
+      require('karma-junit-reporter'),
+      require('karma-jasmine-html-reporter'),
+      require('karma-coverage-istanbul-reporter'),
+      require('@angular/cli/plugins/karma')
+    ],
+    client:{
+      clearContext: false // leave Jasmine Spec Runner output visible in browser
+    },
+    coverageIstanbulReporter: {
+      reports: [ 'html', 'lcovonly' ],
+      fixWebpackSourcePaths: true
+    },
+    angularCli: {
+      environment: 'dev'
+    },
+    reporters: ['progress', 'kjhtml', 'junit'],
+    port: 9876,
+    colors: true,
+    logLevel: config.LOG_INFO,
+    autoWatch: true,
+    browsers: ['PhantomJS'],
+    singleRun: true, // if set to false, Karma should automatically restart any time you save changes to your tests or code. Left on true because of Jenkins.
+    junitReporter    : {
+      outputDir : 'target/surefire-reports/'
+    },
+    files: [
+      'src/app/jasmine.spec.js'
+    ],
+    exclude: [
+    ]
+  });
+};
