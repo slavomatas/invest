25af4080dbca2199d9d7476d08fd7768070162da
Martin Hauskrecht <hauskrechtmartin@gmail.com>
2017-11-27 12:36:53 +0100

    INVEST-178: Added REST for getting user details

--- server/src/main/java/sk/ystad/services/ResourceController.java
+++ server/src/main/java/sk/ystad/services/ResourceController.java
@@ -1,11 +1,15 @@
 package sk.ystad.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import sk.ystad.model.users.database_objects.User;
+import sk.ystad.model.users.repositores.UserRepository;
 
 import java.security.Principal;
 
@@ -13,22 +17,16 @@ import java.security.Principal;
  * Created by nydiarra on 06/05/17.
  */
 @RestController
-@RequestMapping("/springjwt")
+@RequestMapping("/v1")
 public class ResourceController {
-    @RequestMapping(value ="/cities", method = RequestMethod.GET)
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @RequestMapping(value ="/user", method = RequestMethod.GET)
     @PreAuthorize("hasAuthority('ADMIN_USER') or hasAuthority('STANDARD_USER')")
-    public String getUser(Principal principal){
-        return "A";
-    }
-
-    @RequestMapping(value ="/users", method = RequestMethod.GET)
-    @PreAuthorize("hasAuthority('ADMIN_USER')")
-    public String getUsers(@AuthenticationPrincipal User customUser){
-        return "B";
-    }
-
-    @RequestMapping("/test")
-    public String test() {
-        return "test";
+    public User getUser(Principal principal){
+        User user = userRepository.findByUsername(principal.getName());
+        return user;
     }
 }
