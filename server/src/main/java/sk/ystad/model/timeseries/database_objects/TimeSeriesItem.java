b1e4339bad0e2b048a7751aa08ef88360136d887
Tomas Gabrs <gabrstomas@gmail.com>
2017-12-11 23:52:38 +0100

    INVEST-201 Add generic REST for portfolio measurements

---
+++ server/src/main/java/sk/ystad/model/timeseries/database_objects/TimeSeriesItem.java
@@ -0,0 +1,28 @@
+package sk.ystad.model.timeseries.database_objects;
+
+public class TimeSeriesItem {
+
+    private String name;
+    private String value;
+
+    public TimeSeriesItem(String name, String value) {
+        this.name = name;
+        this.value = value;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+}
--- server/src/main/java/sk/ystad/services/PortfolioMeasurementsService.java
+++ server/src/main/java/sk/ystad/services/PortfolioMeasurementsService.java
@@ -1,21 +1,21 @@
 package sk.ystad.services;
 
 import io.swagger.annotations.ApiOperation;
-import org.json.JSONArray;
-import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.web.bind.annotation.*;
 import sk.ystad.model.measures.database_objects.ImmutableMeasure;
 import sk.ystad.model.measures.repositores.PortfolioMeasurementRepository;
+import sk.ystad.model.timeseries.database_objects.TimeSeriesItem;
 import sk.ystad.model.timeseries.database_objects.date.localdate.LocalDateDoubleTimeSeries;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
-@RequestMapping("/measurements")
 public class PortfolioMeasurementsService {
     private static DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
 
@@ -27,10 +27,14 @@ public class PortfolioMeasurementsService {
     }
 
     @CrossOrigin(origins = "*")
-    @GetMapping("/cumulative_measurement")
-    @ApiOperation(value = "Get cumulative measurements for portfolio", notes = "Array of cumulative measurements for given portfolio can be provided.")
-    public String cumulativeMeasurement(@RequestParam(value = "portfolioId") String portfolioId, @RequestParam(value="dateFrom", required=false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime dateFrom,
-                               @RequestParam(value="dateTo", required=false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime dateTo) throws Exception {
+    @ApiOperation(value = "Get cumulative measurements for portfolio", notes = "Array of cumulative measurements of given measurement type for given portfolio can be provided.")
+    @GetMapping("/measurements/portfolios/{portfolioId}/{measurementType}")
+    public List<TimeSeriesItem> getMeasurement(@PathVariable("portfolioId") String portfolioId,
+                                               @PathVariable("measurementType") String measurementType,
+                                               @RequestParam(value="dateFrom", required=false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime dateFrom,
+                                               @RequestParam(value="dateTo", required=false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime dateTo) throws Exception {
+        List<TimeSeriesItem> timeSeriesItems = new ArrayList<>();
+
         LocalDate localDateFrom = null;
         LocalDate localDateTo = null;
         if (dateFrom != null) {
@@ -40,19 +44,17 @@ public class PortfolioMeasurementsService {
             localDateTo = dateTo.toLocalDate();
         }
 
-        ImmutableMeasure immutableMeasure = ImmutableMeasure.of("PORTFOLIO_CUMULATIVE_RETURN");
+        ImmutableMeasure immutableMeasure = ImmutableMeasure.of(measurementType);
         LocalDateDoubleTimeSeries timeSeries = portfolioMeasurementRepository.findMeasure(portfolioId, immutableMeasure, localDateFrom, localDateTo);
 
-        JSONArray dataJson = new JSONArray();
-
         for (int i = 0; i < timeSeries.size(); i++) {
-            JSONObject tmpObject = new JSONObject();
-            tmpObject.put("name", timeSeries.times().get(i));
-            tmpObject.put("value", timeSeries.values().get(i));
-            dataJson.put(tmpObject);
+            LocalDate name = timeSeries.times().get(i);
+            Double value = timeSeries.values().get(i);
+            TimeSeriesItem item = new TimeSeriesItem(name.toString(), value.toString());
+            timeSeriesItems.add(item);
         }
 
-        return dataJson.toString();
+        return timeSeriesItems;
     }
 
 }
