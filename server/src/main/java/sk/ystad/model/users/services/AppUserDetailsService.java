fe15ae46c8dd94a01bcec5c5595a34040fc55624
Filip Sukenik <murtyyff@gmail.com>
2017-12-12 13:43:47 +0100

    Added email sending

--- server/src/main/java/sk/ystad/model/users/services/AppUserDetailsService.java
+++ server/src/main/java/sk/ystad/model/users/services/AppUserDetailsService.java
@@ -11,6 +11,7 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 import sk.ystad.common.data_structures.Response;
+import sk.ystad.common.mail.EmailService;
 import sk.ystad.model.users.database_objects.User;
 import sk.ystad.model.users.repositores.UserRepository;
 
@@ -22,13 +23,20 @@ import java.util.List;
 public class AppUserDetailsService implements UserDetailsService {
 
     private static final long REGISTRATION_TIME_OUT = 1000 * 60 * 60 * 12;
-    @Autowired
-    private UserRepository userRepository;
+    private final UserRepository userRepository;
 
-    @Autowired
-    private PasswordEncoder passwordEncoder;
+    private final PasswordEncoder passwordEncoder;
+
+    private final EmailService emailService;
 
     static Logger log = Logger.getLogger(AppUserDetailsService.class.getName());
+
+    @Autowired
+    public AppUserDetailsService(UserRepository userRepository, PasswordEncoder passwordEncoder, EmailService emailService) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+        this.emailService = emailService;
+    }
 
     @Override
     public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
@@ -53,13 +61,20 @@ public class AppUserDetailsService implements UserDetailsService {
         if (emailExist(user.getEmail()) || usernameExist(user.getUsername())) {
             return new Response(false, "User already exists");
         }
-        while(tokenExists(user.getRegistrationToken())){
+        while (tokenExists(user.getRegistrationToken())) {
             user.generateNewToken();
         }
         user.setRegistrationTimestamp(new Date());
         hashPassword(user);
         userRepository.save(user);
+        sendEmail(user);
         return new Response(true, null);
+    }
+
+    private void sendEmail(User user) {
+        emailService.sendMail(user.getEmail(), "Registration confirmation", "Thank you for your " +
+                "registration.\nPlease confirm your email by clicking on following link:\n" +
+                "http://localhost:8085/tuBudeLink/" + user.getRegistrationToken());
     }
 
     private boolean tokenExists(String registrationToken) {
@@ -93,13 +108,13 @@ public class AppUserDetailsService implements UserDetailsService {
 
     public Response checkUser(String token) {
         User user = userRepository.findByRegistrationToken(token);
-        if(user == null) {
+        if (user == null) {
             return new Response(false, null);
         } else {
-            if(user.isRegistrationConfirmed()){
+            if (user.isRegistrationConfirmed()) {
                 return new Response(true, null);
             } else {
-                if(registrationExpired(user.getRegistrationTimestamp())){
+                if (registrationExpired(user.getRegistrationTimestamp())) {
                     userRepository.delete(user);
                     return new Response(false, null);
                 } else {
@@ -112,22 +127,23 @@ public class AppUserDetailsService implements UserDetailsService {
     }
 
     private boolean registrationExpired(Date registrationTimestamp) {
-        return  (new Date().getTime() - registrationTimestamp.getTime()) > REGISTRATION_TIME_OUT;
+        return (new Date().getTime() - registrationTimestamp.getTime()) > REGISTRATION_TIME_OUT;
     }
 
     /**
      * Method for removing users with expired registration token
      */
-    @Scheduled(cron="0 0 */6 * * *")
+    @Scheduled(cron = "0 0 */6 * * *")
     public void removeUnActivatedUsers() {
         List<User> inactiveUsers = userRepository.findByRegistrationConfirmedFalse();
         //Check all users for expired tokens
-        for (User user: inactiveUsers) {
+        for (User user : inactiveUsers) {
             // If token is expired, delete the user
-            if(registrationExpired(user.getRegistrationTimestamp())) {
+            if (registrationExpired(user.getRegistrationTimestamp())) {
                 userRepository.delete(user);
                 log.info("Deleted unactived user " + user.getUsername() + " : token expired");
-            };
+            }
+            ;
         }
     }
 }
