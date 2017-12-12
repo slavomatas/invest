56ab9db04eb2ea43564f99151f5be7c50f9745aa
Martin Hauskrecht <hauskrechtmartin@gmail.com>
2017-12-12 17:35:29 +0100

    INVEST-170: Added Swagger comments

--- server/src/main/java/sk/ystad/services/AuthorizationController.java
+++ server/src/main/java/sk/ystad/services/AuthorizationController.java
@@ -1,5 +1,6 @@
 package sk.ystad.services;
 
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
@@ -16,6 +17,7 @@ public class AuthorizationController {
 
     @CrossOrigin(origins = "*")
     @PostMapping("/register")
+    @ApiOperation(value = "Register new user", notes = "Registers new user")
     public Response register(@RequestParam(value = "password") String password, @RequestParam(value = "surname") String surname,
                          @RequestParam(value = "name") String name, @RequestParam(value = "email") String email) {
 
@@ -25,7 +27,8 @@ public class AuthorizationController {
 
     @CrossOrigin(origins = "*")
     @PostMapping("/register/{token}")
-    public Response register(@PathVariable String token) {
+    @ApiOperation(value = "Verify registration token", notes = "Verifies if the submitted token is valid")
+    public Response verify(@PathVariable String token) {
         return appUserDetailsService.checkUser(token);
     }
 }
