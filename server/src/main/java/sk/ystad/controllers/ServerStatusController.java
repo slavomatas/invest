e8ebbbe45fba822a2b06ff1d4318f5060e8012d9
Filip Sukenik <murtyyff@gmail.com>
2018-02-08 13:54:16 +0100

    INVEST-263 logc fix

--- server/src/main/java/sk/ystad/controllers/ServerStatusController.java
+++ server/src/main/java/sk/ystad/controllers/ServerStatusController.java
@@ -2,12 +2,10 @@ package sk.ystad.controllers;
 
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-import sk.ystad.common.data_structures.ServerStatus;
 import sk.ystad.services.ServerStatusService;
 
 @RestController
@@ -26,8 +24,8 @@ public class ServerStatusController {
 
     @RequestMapping(method = RequestMethod.GET)
     @ApiOperation(value = "Get server status", notes = "Determines if the server is running or not")
-    public ResponseEntity<ServerStatus> getServerStatus() {
-        return new ResponseEntity<>(serverStatusService.getServerStatus(), HttpStatus.OK);
+    public ResponseEntity getServerStatus() {
+        return serverStatusService.getServerStatus();
     }
 
 }
