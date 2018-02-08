39bbc78a2aff32808e07121b01aff6e3e3e251ca
martin <hauskrechtmartin@gmail.com>
2018-02-08 14:23:37 +0100

    INVEST-262: Add response logging

--- server/src/main/java/sk/ystad/common/logging/LogRequestFilter.java
+++ server/src/main/java/sk/ystad/common/logging/LogRequestFilter.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.context.request.ServletRequestAttributes;
 import org.springframework.web.filter.OncePerRequestFilter;
 import org.springframework.web.util.ContentCachingRequestWrapper;
+import org.springframework.web.util.ContentCachingResponseWrapper;
 import org.springframework.web.util.WebUtils;
 
 import javax.servlet.FilterChain;
@@ -26,91 +27,80 @@ import java.util.Map;
  *
  */
 @Component
-public class LogRequestFilter extends OncePerRequestFilter implements Ordered {
+public class LogRequestFilter extends OncePerRequestFilter {
 
-    private final Logger logger = LogManager.getLogger(getClass());
+    private boolean includeResponsePayload = true;
+    private int maxPayloadLength = 1000;
 
-    // put filter at the end of all other filters to make sure we are processing after all others
-    private int order = Ordered.LOWEST_PRECEDENCE - 8;
-    private ErrorAttributes errorAttributes;
-
-    @Override
-    public int getOrder() {
-        return order;
-    }
-
-    @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
-            throws ServletException, IOException {
-        ContentCachingRequestWrapper wrappedRequest = new ContentCachingRequestWrapper(request);
-        int status = HttpStatus.INTERNAL_SERVER_ERROR.value();
-
-        // pass through filter chain to do the actual request handling
-        filterChain.doFilter(wrappedRequest, response);
-        status = response.getStatus();
-
-        Map<String, Object> trace = getTrace(wrappedRequest, status);
-
-        // body can only be read after the actual request handling was done!
-        getBody(wrappedRequest, trace);
-        logTrace(wrappedRequest, trace);
-
-    }
-
-    private void getBody(ContentCachingRequestWrapper request, Map<String, Object> trace) {
-        // wrap request to make sure we can read the body of the request (otherwise it will be consumed by the actual
-        // request handler)
-        ContentCachingRequestWrapper wrapper = WebUtils.getNativeRequest(request, ContentCachingRequestWrapper.class);
-        if (wrapper != null) {
-            byte[] buf = wrapper.getContentAsByteArray();
-            if (buf.length > 0) {
-                String payload;
-                try {
-                    payload = new String(buf, 0, buf.length, wrapper.getCharacterEncoding());
-                }
-                catch (UnsupportedEncodingException ex) {
-                    payload = "[unknown]";
-                }
-
-                trace.put("body", payload);
-            }
-        }
-    }
-
-    private void logTrace(HttpServletRequest request, Map<String, Object> trace) {
-        Object method = trace.get("method");
-        Object path = trace.get("path");
-        Object statusCode = trace.get("statusCode");
-
-        logger.info(String.format("%s %s produced an status code '%s'. Trace: '%s'", method, path, statusCode,
-                trace));
-
-    }
-
-    protected Map<String, Object> getTrace(HttpServletRequest request, int status) {
-        Throwable exception = (Throwable) request.getAttribute("javax.servlet.error.exception");
-
-        Principal principal = request.getUserPrincipal();
-
-        Map<String, Object> trace = new LinkedHashMap<String, Object>();
-        trace.put("method", request.getMethod());
-        trace.put("path", request.getRequestURI());
+    private String getContentAsString(byte[] buf, int maxLength, String charsetName) {
+        if (buf == null || buf.length == 0) return "";
+        int length = Math.min(buf.length, this.maxPayloadLength);
         try {
-            trace.put("principal", principal.getName());
+            return new String(buf, 0, length, charsetName);
+        } catch (UnsupportedEncodingException ex) {
+            return "Unsupported Encoding";
         }
-        catch (Exception e) {
-            logger.warn("Unauthorized service call: " + e.getMessage());
+    }
+
+    /**
+     * Log each request and respponse with full Request URI, content payload and duration of the request in ms.
+     * @param request the request
+     * @param response the response
+     * @param filterChain chain of filters
+     * @throws ServletException
+     * @throws IOException
+     */
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        long startTime = System.currentTimeMillis();
+        StringBuffer reqInfo = new StringBuffer()
+                .append("[")
+                .append(startTime % 10000)  // request ID
+                .append("] ")
+                .append(request.getMethod())
+                .append(" ")
+                .append(request.getRequestURL());
+
+        String queryString = request.getQueryString();
+        if (queryString != null) {
+            reqInfo.append("?").append(queryString);
         }
 
-        trace.put("query", request.getQueryString());
-        trace.put("statusCode", status);
-
-        if (exception != null && this.errorAttributes != null) {
-            trace.put("error", this.errorAttributes
-                    .getErrorAttributes(new ServletRequestAttributes(request), true));
+        if (request.getAuthType() != null) {
+            reqInfo.append(", authType=")
+                    .append(request.getAuthType());
+        }
+        if (request.getUserPrincipal() != null) {
+            reqInfo.append(", principalName=")
+                    .append(request.getUserPrincipal().getName());
         }
 
-        return trace;
+        this.logger.info("=> " + reqInfo);
+
+        // ========= Log request and response payload ("body") ========
+        // We CANNOT simply read the request payload here, because then the InputStream would be consumed and cannot be read again by the actual processing/server.
+        //    String reqBody = DoogiesUtil._stream2String(request.getInputStream());   // THIS WOULD NOT WORK!
+        // So we need to apply some stronger magic here :-)
+        ContentCachingRequestWrapper wrappedRequest = new ContentCachingRequestWrapper(request);
+        ContentCachingResponseWrapper wrappedResponse = new ContentCachingResponseWrapper(response);
+
+        filterChain.doFilter(wrappedRequest, wrappedResponse);     // ======== This performs the actual request!
+        long duration = System.currentTimeMillis() - startTime;
+
+        // I can only log the request's body AFTER the request has been made and ContentCachingRequestWrapper did its work.
+        String requestBody = this.getContentAsString(wrappedRequest.getContentAsByteArray(), this.maxPayloadLength, request.getCharacterEncoding());
+        if (requestBody.length() > 0) {
+            this.logger.info("   Request body: " +requestBody);
+        }
+
+        this.logger.debug("<= " + reqInfo + ": returned status=" + response.getStatus() + " in "+duration + "ms");
+        if (includeResponsePayload) {
+            byte[] buf = wrappedResponse.getContentAsByteArray();
+            this.logger.info("   Response body: "+getContentAsString(buf, this.maxPayloadLength, response.getCharacterEncoding()));
+        }
+
+        wrappedResponse.copyBodyToResponse();  // IMPORTANT: copy content of response back into original response
+
     }
 
 }
