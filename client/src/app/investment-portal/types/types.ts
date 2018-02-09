10eb8e3426fb4ddd0f77cd4a85e58b5b120c270b
michal.staskovan <mmmm939@gmail.com>
2018-02-09 14:14:05 +0100

    INVEST-268 Add PortfolioPosition type

--- client/src/app/investment-portal/types/types.ts
+++ client/src/app/investment-portal/types/types.ts
@@ -32,11 +32,7 @@ export interface PortfolioDetails {
   cash: number;
   closed: boolean;
   isDisplayed: boolean;
-  positions:
-    {
-      symbol: string;
-      value: number;
-    }[];
+  positions: PortfolioPosition[];
   series:                 // series - time series of the portfolio
     {
       name: string,
@@ -51,6 +47,27 @@ export interface PortfolioTimeSeries {
     {
       name: string,
       value: number
+    }[];
+}
+
+export interface PortfolioPosition {
+  symbol: string;
+  value: number;    // market value of position - used on dashboard in portfolio overview list
+  name: string;
+  quantity: number; // sum trades.amount
+  price: number;
+  currency: string;
+  priceLast20Days: {
+    name: string;
+    value: number;
+  }[];
+  marketValue: number;
+  lastChange: number;
+  trades: {
+      tradeId: number
+      price: number;
+      amount: number;
+      dateTime: Date;
     }[];
 }
 
