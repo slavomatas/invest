cbbc185787420189630143af2aa49e5a52046ddf
Lukas Menhert <lmenhert94@gmail.com>
2018-03-01 20:01:55 +0100

    Fix displaying NO CASH when the number in portfolio list row is very small.

--- client/src/app/investment-portal/components/portfolios/portfolio-list-row/portfolio-list-row.component.ts
+++ client/src/app/investment-portal/components/portfolios/portfolio-list-row/portfolio-list-row.component.ts
@@ -44,7 +44,6 @@ export class PortfolioListRowComponent implements OnInit, OnChanges {
         });
       }
       this.portfolioData.marketValue = parseFloat(this.portfolioData.marketValue.toFixed(2));
-      this.portfolioData.cash = this.portfolioData.cash < 0.0001 ? this.portfolioData.cash : parseFloat(this.portfolioData.cash.toFixed(4));
       this.portfolioData.lastChangeAbs = parseFloat(this.portfolioData.lastChangeAbs.toFixed(4));
       this.portfolioData.lastChangePct = parseFloat(this.portfolioData.lastChangePct.toFixed(4));
     }
@@ -81,6 +80,10 @@ export class PortfolioListRowComponent implements OnInit, OnChanges {
   descShowMore(){
     this.limit = 2500;
     this.descMoreShown = true;
+  }
+
+  formatSmallNumber(input: number){
+    return input >= 0.0001 ? String(input) : "0.000";
   }
 
 }
