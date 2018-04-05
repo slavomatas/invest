4683c01e86c51cab84e8136cd539017c1acaed60
Tomas Gabrs <gabrstomas@gmail.com>
2018-04-05 17:57:42 +0200

    INVEST-369 Show error when server is disconnected

--- client/src/app/investment-portal/services/websocket/message.service.ts
+++ client/src/app/investment-portal/services/websocket/message.service.ts
@@ -7,9 +7,11 @@ import { NgRedux } from '@angular-redux/store';
 import { AppState } from '../../store/store';
 import { User } from '../../types/types';
 import { PortfolioService } from '../portfolio/portfolio.service';
+import { MessageBarService } from '../../message-bar.service';
 
 @Injectable()
 export class MessageService {
+  self = this;
 
   private messageSource = new Subject<string>();
   messageReceived$ = this.messageSource.asObservable();
@@ -18,10 +20,12 @@ export class MessageService {
 
   user$ = this.ngRedux.select(state => state.user);
   userEmail;
+  messageId: number = null;
 
   constructor(
     private ngRedux: NgRedux<AppState>,
-    private portfolioService: PortfolioService
+    private portfolioService: PortfolioService,
+    private messageBarService: MessageBarService
   ) {
     this.user$.subscribe((data: User) => {
       if (data != null) {
@@ -37,15 +41,32 @@ export class MessageService {
       this.stompClient.subscribe('/user/queue/messages', (message: Message) => {
         this.onMessage(message);
       });
+      if (this.messageId != null) {
+        this.messageBarService.removeMessage(this.messageId);
+        this.messageId = null;
+      }
       this.subscribeToDefaults();
+    }, () => {
+      if (this.messageId == null) {
+        this.messageId = this.messageBarService.addMessage('Disconnected');
+      }
+      setTimeout(() => {
+        this.connect();
+      }, 5000);
     });
+
+    socket.onerror = () => {
+      if (this.messageId == null) {
+        this.messageId = this.messageBarService.addMessage('Disconnected');
+      }
+      this.connect();
+    };
   }
 
    public sendMessage(message) {
 
       this.stompClient.send('/app/send/message', {'email': this.userEmail, 'user':
       JSON.stringify(this.ngRedux.getState().user)}, message);
-      console.log(JSON.stringify(this.ngRedux.getState().user));
    }
 
   public subscribeToDefaults() {
