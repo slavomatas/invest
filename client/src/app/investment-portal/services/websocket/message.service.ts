fd7b6193dc2aa614bef7732cd4892dc7aafb3058
Tomas Gabrs <gabrstomas@gmail.com>
2018-04-05 16:18:22 +0200

    INVEST-368 Refresh data when calculation is done

--- client/src/app/investment-portal/services/websocket/message.service.ts
+++ client/src/app/investment-portal/services/websocket/message.service.ts
@@ -1,11 +1,12 @@
-import {Injectable} from '@angular/core';
-import {Client, Frame, Message} from 'stompjs';
+import { Injectable } from '@angular/core';
+import { Client, Frame, Message } from 'stompjs';
 import * as stompjs from 'stompjs';
 import * as SockJS from 'sockjs-client';
-import {Subject} from 'rxjs/Subject';
+import { Subject } from 'rxjs/Subject';
 import { NgRedux } from '@angular-redux/store';
 import { AppState } from '../../store/store';
 import { User } from '../../types/types';
+import { PortfolioService } from '../portfolio/portfolio.service';
 
 @Injectable()
 export class MessageService {
@@ -19,20 +20,24 @@ export class MessageService {
   userEmail;
 
   constructor(
-    private ngRedux: NgRedux<AppState>
+    private ngRedux: NgRedux<AppState>,
+    private portfolioService: PortfolioService
   ) {
+    this.user$.subscribe((data: User) => {
+      if (data != null) {
+        this.userEmail = data.email;
+      }
+    });
+  }
+
+  public connect() {
     const socket = new SockJS('api/socket') as WebSocket;
     this.stompClient = stompjs.over(socket);
     this.stompClient.connect({}, (frame: Frame) => {
       this.stompClient.subscribe('/user/queue/messages', (message: Message) => {
         this.onMessage(message);
       });
-    });
-
-    this.user$.subscribe((data: User) => {
-      if (data != null) {
-        this.userEmail = data.email;
-      }
+      this.subscribeToDefaults();
     });
   }
 
@@ -43,9 +48,22 @@ export class MessageService {
       console.log(JSON.stringify(this.ngRedux.getState().user));
    }
 
+  public subscribeToDefaults() {
+    this.sendMessage('{"command":"subscribe_to","values":[{"event":"portfolios_change"}]}');
+  }
 
   private onMessage(message: Message) {
-    console.log('Received message:', message.body);
+    const command = JSON.parse(message.body);
+    switch (command.command) {
+      case 'update_portfolio': {
+        const portfolioId = command.portfolio_id;
+        this.portfolioService.refreshPortfolioData(Number.parseInt(portfolioId));
+        break;
+      }
+      default: {
+        break;
+      }
+    }
   }
 
 
