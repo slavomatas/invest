36bd7d268129cd97ea56549f15092ab2684a650e
Tomas Gabrs <gabrstomas@gmail.com>
2018-04-12 16:17:09 +0200

    INVEST-403 Hide error message insertNode when dashboard is loading.

--- client/src/app/investment-portal/services/global-error-handler/global-error-handler.ts
+++ client/src/app/investment-portal/services/global-error-handler/global-error-handler.ts
@@ -2,13 +2,20 @@ import { ErrorHandler, Injectable, Injector} from '@angular/core';
 import { MessageBarService } from '../../message-bar.service';
 import { environment } from '../../../../environments/environment';
 import * as Raven from 'raven-js';
+import { Router } from '@angular/router';
 
 @Injectable()
 export class GlobalErrorHandler implements ErrorHandler {
   constructor(private injector: Injector) { }
   handleError(error) {
+    const router = this.injector.get(Router);
     const messageService = this.injector.get(MessageBarService);
-    messageService.addMessage('Whops, something went wrong.', 5000);
+
+    // This is Angular bug, not fixed in Angular 5.2.9 yet
+    // Do not show message when app is on dashboard and error message is <<check in next line>>
+    if (!(router.routerState.snapshot.url === '/dashboard' && error.message === `Cannot read property 'insertNode' of undefined`)) {
+      messageService.addMessage('Whops, something went wrong.', 5000);
+    }
 
     if (environment.production) {
       Raven.captureException(error);
