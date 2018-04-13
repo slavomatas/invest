c7e7dc3a73b36ccb294ad55e15120b0f7d4867a7
michal.staskovan <mmmm939@gmail.com>
2018-04-13 16:00:16 +0200

    INVEST-391 change layout of dashboard - portfolios in two columns and horizontal bar chart at the bottom

--- client/src/app/investment-portal/components/dashboard/dashboard-portfolio-list/dashboard-portfolio-list.component.ts
+++ client/src/app/investment-portal/components/dashboard/dashboard-portfolio-list/dashboard-portfolio-list.component.ts
@@ -1,11 +1,13 @@
 import { Component, Input, SimpleChanges, OnChanges } from '@angular/core';
 import { PortfolioDetails} from '../../../types/types';
 import { getDisplayedPortfolios } from '../../../utils/portfolio-utils';
+import { fuseAnimations } from '../../../../core/animations';
 
 @Component({
   selector: 'invest-dashboard-portfolio-list',
   templateUrl: './dashboard-portfolio-list.component.html',
-  styleUrls: ['./dashboard-portfolio-list.component.scss']
+  styleUrls: ['./dashboard-portfolio-list.component.scss'],
+  animations: fuseAnimations
 })
 export class DashboardPortfolioListComponent implements OnChanges {
   @Input() dashboardPortfolioList: PortfolioDetails[];
