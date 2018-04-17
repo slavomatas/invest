8f9ee2d6b4b70d5b9eceb5f734dc943913db8b3a
michal.staskovan <mmmm939@gmail.com>
2018-04-17 14:04:11 +0200

    INVEST-395 add texts to the tour

--- client/src/app/investment-portal/toures/tour-definitions.ts
+++ client/src/app/investment-portal/toures/tour-definitions.ts
@@ -26,29 +26,29 @@ export const firstLoginTour: IStepOption[] = [
 export const demandDashboardTour: IStepOption[] = [
   {
     anchorId: 'dashboard-step-1',
-    title: 'Welcome...',
-    content: 'Here you can see...'
+    title: 'Market value',
+    content: 'See the difference in how the value of your portfolios has changed in the selected period. In other words, whether you gain or lose.'
   },
    {
     anchorId: 'dashboard-step-2',
-    title: 'Welcome...',
-    content: 'Here you can see...'
+    title: 'Period',
+    content: 'Choose the period you\'d like to watch the progress of your portfolios.'
   },
   {
     anchorId: 'dashboard-step-3',
-    title: 'Welcome...',
-    content: 'Here you can see...',
+    title: 'Portfolios',
+    content: 'Select portfolios that interest you most.',
   },
   {
     anchorId: 'dashboard-step-4',
-    title: 'Welcome...',
-    content: 'Here you can see...',
+    title: 'Portfolios comparison',
+    content: 'See the difference in gain or loss of your portfolios in last days.',
     preventScrolling:	true
   },
   {
     anchorId: 'dashboard-step-5',
-    title: 'Welcome...',
-    content: 'Here you can see...',
+    title: 'Portfolios overview',
+    content: 'The portfolio consists of several securities, such as Apple, Google, etc. The amount of security you\'ve bought is called position. Here you can see the overview of your portfolios and it\'s percentual composition by securities. \n For more detailed view click on the chart.',
     preventScrolling:	true
   }
 ];
@@ -56,77 +56,64 @@ export const demandDashboardTour: IStepOption[] = [
 export const demandPortfolioListTour: IStepOption[] = [
   {
     anchorId: 'portfolio-list-step-1',
-    title: 'Welcome...',
-    content: 'Here you can see...'
+    title: 'Portfolios\' filter',
+    content: 'Filter the portfolios you want to see, whether it\'s your private or public modeled portfolios.'
   },
   {
     anchorId: 'portfolio-list-step-2',
-    title: 'Welcome...',
-    content: 'Here you can see...'
+    content: 'This is a detailed list of portfolios. You can see portfolios\' market value, last change of the value and the description.'
   },
   {
     anchorId: 'portfolio-list-step-3',
-    title: 'Welcome...',
-    content: 'Here you can see...'
+    content: 'This chart shows the composition of the portfolio based on the market value of each position.'
   },
   {
     anchorId: 'portfolio-list-step-4',
-    title: 'Welcome...',
-    content: 'Here you can see...'
+    content: 'See the detailed view of the portfolio.'
   },
   {
     anchorId: 'portfolio-list-step-5',
-    title: 'Welcome...',
-    content: 'Here you can see...'
+    content: 'You can disable the portfolio, so it\'ll no longer show on your dashboard. This change is of course revertable. '
   }
 ];
 
 export const demandPortfolioDetailTour: IStepOption[] = [
   {
     anchorId: 'portfolio-detail-step-1',
-    title: 'Welcome...',
-    content: 'Here you can see...'
+    content: 'Shows the actual market value of the portfolio.'
   },
   {
     anchorId: 'portfolio-detail-step-2',
-    title: 'Welcome...',
-    content: 'Here you can see...'
+    content: 'Shows the portfolio change from yesterday.'
   },
   {
     anchorId: 'portfolio-detail-step-3',
-    title: 'Welcome...',
-    content: 'Here you can see...'
+    content: 'Shows the change from last week, month, year and from the creation of the portfolio.'
   },
   {
     anchorId: 'portfolio-detail-step-4',
-    title: 'Welcome...',
-    content: 'Here you can see...'
+    content: 'Shows how many positions you have in concrete security. '
   },
   {
     anchorId: 'portfolio-detail-step-5',
-    title: 'Welcome...',
-    content: 'Here you can see...'
+    content: 'Here you can see the progress of securities in last 20 days. '
   },
   {
     anchorId: 'portfolio-detail-step-6',
-    title: 'Welcome...',
-    content: 'Here you can see...'
+    content: 'Here you can see the last change in your concrete positions. '
   },
   {
     anchorId: 'portfolio-detail-step-7',
-    title: 'Welcome...',
-    content: 'Here you can see...'
+    content: 'You can buy or sell positions right here. '
   },
   {
     anchorId: 'portfolio-detail-step-8',
-    title: 'Welcome...',
-    content: 'Here you can see...',
+    content: 'Transactions are individual purchases of the position of a security, here you can see the price at which you made the transaction.',
     preventScrolling: false
   },
   {
     anchorId: 'portfolio-detail-step-9',
-    title: 'Welcome...',
-    content: 'Here you can see...',
+    content: 'You   can edit details of transactions right here.',
     preventScrolling: false
   }
 ];
